177a03faac56df1f8c914a01b94e4758069cbfbe
aneesh2001 <88796746+aneesh2001@users.noreply.github.com>
2022-02-06 17:56:56 -0800

    Updated second stepper motor setup

--- sonic-bloom-main/sonic-bloom-main.ino
+++ sonic-bloom-main/sonic-bloom-main.ino
@@ -11,12 +11,14 @@ int thresh_val = 990;
 int photoVal_1;
 
 // Stepper Motor Variables
-Stepper myStepper(StepsPerRevolution, 8, 10, 9, 11);
+Stepper myStepper_1(StepsPerRevolution, 8, 10, 9, 11);
+Stepper myStepper_2(StepsPerRevolution, 2, 4, 3, 5);
 
 // SETUP
 void setup() {
   Serial.begin(9600);
-  myStepper.setSpeed(motSpeed); // Stepper Motor 
+  myStepper_1.setSpeed(motSpeed); // Stepper Motor 1
+  myStepper_2.setSpeed(motSpeed);
   pinMode(photoPin_1, INPUT);  // Photoresistor I
 }
 
@@ -27,9 +29,9 @@ void loop() {
 
   if(photoVal_1 <= thresh_val) // Checks if light detect past threshold.
     { 
-      myStepper.step(StepsPerRevolution/2);
+      myStepper_1.step(StepsPerRevolution/2);
       delay(dt);
-      myStepper.step(-StepsPerRevolution/2);
+      myStepper_2.step(-StepsPerRevolution/2);
       delay(dt);
     }
   else{}
